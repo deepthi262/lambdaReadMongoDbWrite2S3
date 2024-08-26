155023cb8379ee683c83be3d74e130937615cd9b
aasoftnet <AASoftwareNetworkingInc@gmail.com>
2024-08-26 18:25:36 -0500

    modified

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 //  const { handler } = require('./index');
- const {handler} = require('./indexChunks');
+//const {handler} = require('./readWriteToFile');
 // const { handler } = require('./recursive');
-
+const {handler} =require('./readWriteToS3');
 const event = {test: "true"}; // Simulate the event object your Lambda function would receive
 
 handler(event).then(response => {
